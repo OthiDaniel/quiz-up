ef5a64a7dea84cccedb1ce26883f3521d1398e8c
OthiDaniel <othidaniel@gmail.com>
2018-12-10 11:20:30 +0300

    write function in .js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function score(){
+  var questionOne = document.quiz.questionOne.value;
+  var questionTwo = document.quiz.questionTwo.value;
+  var questionThree = document.quiz.questionThree.value;
+  var questionFour = document.quiz.questionFour.value;
+  var questionFive = document.quiz.questionFive.value;
+  var correct = 0;
+
+}
